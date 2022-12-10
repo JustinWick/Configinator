23840724281b090dd0d2df9828c1d78589ebc03b
Loki SM <lokism42069@gmail.com>
2022-12-10 17:45:31 -0500

    Initial working version

---
+++ Configinator/Configinator.fs
@@ -0,0 +1,39 @@
+﻿module Configinator.Configinator
+
+open System.IO
+open System.Text.RegularExpressions
+
+type ConfigFile = {
+    Source   : string
+    Lines    : string []
+    Entries  : Map<string, string>
+}
+
+module ConfigFile =
+    
+    let private entryRegex = Regex(@"^([^\=]+)\=([^#]*)(#.*)?$", RegexOptions.Compiled)
+    
+    let fromLines (source: string) (linesSeq: string seq) =
+        let lines = linesSeq |> Array.ofSeq
+        let entries =
+            lines
+            |> Seq.choose (fun line -> let m = entryRegex.Match line in if m.Success then (m.Groups[1].Value.Trim(), m.Groups[2].Value.Trim()) |> Some else None)
+            |> Map.ofSeq<string, string>
+        {
+            Source  = source
+            Lines   = lines
+            Entries = entries
+        }
+        
+        
+    
+    let fromFilepath (filepath: string) = filepath |> File.ReadLines |> fromLines filepath
+    
+    let getAs func key configFile = configFile.Entries[key] |> func
+    
+    let getString = getAs id
+    
+    let getInt = getAs int
+    
+    let getFloat = getAs float
+    
---
+++ Configinator/Program.fs
@@ -0,0 +1,24 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open Configinator
+open Configinator.Configinator
+
+let configFile = "example.txt" |> Configinator.ConfigFile.fromFilepath
+
+printfn "Raw values:"
+configFile.Entries |> Map.iter (fun key value -> printfn $"{key}:\t{value}")
+    
+    
+let exampleString = configFile |> ConfigFile.getString "exampleString"
+let exampleInt    = configFile |> ConfigFile.getInt    "exampleInt"
+let exampleFloat  = configFile |> ConfigFile.getFloat  "exampleFloat"
+
+printfn $""
+printfn $""
+printfn $"Parsed values:"
+printfn $"exampleString:\t{exampleString}"
+printfn $"exampleInt:\t{exampleInt}"
+printfn $"exampleFloat:\t{exampleFloat}"
+
+
+
